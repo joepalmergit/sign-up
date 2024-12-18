f4674e6613040e00e8005bbebff6c3ee5c35b0c6
Joe Palmer <114061674+joepalmergit@users.noreply.github.com>
2024-12-18 11:17:37 +0000

    Add validation logic

--- process_sign_up_form.php
+++ process_sign_up_form.php
@@ -3,7 +3,30 @@
     $username = $_POST["username"];
     $email_address = $_POST["email_address"];
 
-    // TODO: Validation logic
+    $username = trim($username);
+    $email_address = trim($email_address);
+
+    if (empty($username)) {
+        echo "<p>You must enter a username.</p>";
+        die();
+    }
+
+    $username_length = strlen($username);
+    
+    if ($username_length < 2) {
+        echo "<p>Your username must be at least two characters long.</p>";
+        die();
+    }
+
+    if (empty($email_address)) {
+        echo "<p>You must enter an email address.</p>";
+        die();
+    }
+
+    if(!filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
+        echo "<p>You must enter a valid email address.</p>";
+        die();
+    }
 
     echo "<p>Thank you for registering.</p>";
 
